7e53e64ebf06d1d841852fed28851e13bf88aeee
KatieSil <katiesil@mail.ru>
2024-09-06 01:04:51 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,5 @@
+count_of_completed_HW = 12
+count_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = count_of_hours_spent / count_of_completed_HW
+print('Курс:',course_name,', всего задач:',count_of_completed_HW,', затрачено часов:',count_of_hours_spent,', среднее время выполнения',time_for_one_task,' часа.')
